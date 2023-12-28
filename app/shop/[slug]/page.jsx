79427ddef71b5ab275c9a2bfe38109c5cd1cc3ba
Maxi Vecchio <maximilianovecchio@gmail.com>
2023-12-28 19:43:26 -0300

    a

--- app/shop/[slug]/page.jsx
+++ app/shop/[slug]/page.jsx
@@ -1,8 +1,14 @@
+export async function generateMetadata ({params, searchParams}, parent) {
+  return {
+    title: `MVShop - ${params.slug}`
+  }
+}
+
 import ProductsList from '@/app/shop/components/ProductsList'
 
 const ShopPage = ({ params }) => {
   return (
-    <div className="mt-16">
+    <div className="mt-16 p-4">
       <ProductsList slug={params.slug} />
     </div>
   );
--- app/shop/components/ProductsList.jsx
+++ app/shop/components/ProductsList.jsx
@@ -1,38 +1,38 @@
-'use client'
+"use client";
 import ProductCard from "@/app/components/ProductCard";
 import { useProducts } from "@/app/context/productContext";
 import { useEffect, useState } from "react";
 
-export default function ProductsList({slug}) {
-  
-  const { products: allProducts, getProductsByCategory} = useProducts();
-  const [products, setProducts] = useState([])
+export default function ProductsList({ slug }) {
+  const { products: allProducts, getProductsByCategory } = useProducts();
+  const [products, setProducts] = useState(null);
 
   useEffect(() => {
     // Define una función asincrónica dentro de useEffect
     const fetchProducts = async () => {
-      if (slug === 'all') {
-        console.log('Entre en all');
+      if (slug === "all") {
+        console.log("Entre en all");
         console.log(allProducts);
         setProducts(allProducts);
       } else {
-        console.log('No estoy en all');
-  
+        console.log("No estoy en all"); 
+
         try {
           const productsByCategory = await getProductsByCategory(slug);
-          setProducts(productsByCategory);
-          console.log(productsByCategory);
+          if (!productsByCategory.success) {
+            console.log("a");
+          } else {
+            setProducts(productsByCategory);
+          }
         } catch (error) {
-          console.error("Error al obtener productos por categoría:", error);
-          // Manejo de errores aquí, por ejemplo, establecer un estado de error
+          console.error("Error al obtener productos por categoría.");
         }
       }
     };
-  
+
     // Llama a la función asincrónica
     fetchProducts();
   }, [slug, allProducts]);
-  
 
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 gap-4">
